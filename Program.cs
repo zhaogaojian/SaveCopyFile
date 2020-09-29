3eb78506718ec9e8d443f09723b85878b501ca3c
赵高建 <zhaogaojian@bhuitong.com>
2020-09-29 18:41:58 +0800

    111

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SaveCopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srcDir = args[0];
+            string srcFile = args[1];
+            string destDir = args[2];
+            string tempPath=srcFile.Replace(srcDir, "");
+            try
+            {
+                File.Copy(srcFile, destDir + tempPath,true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
